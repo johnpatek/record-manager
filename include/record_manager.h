b120200958670be8e200da8e0815b7d1ae1b59b2
John R. Patek <johnpatek2@gmail.com>
2020-11-07 22:04:36 -0800

    Added client and server classes

--- include/record_manager.h
+++ include/record_manager.h
@@ -6,4 +6,19 @@
 namespace rmp
 {
     std::string djb_hash(const std::string& data);
+
+    class server
+    {
+    public:
+        server(uint16_t port);
+        ~server();
+    private:
+    };
+
+    class client
+    {
+    public:
+        client(const std::string& host, uint16_t port);
+        ~client();
+    };
 }
