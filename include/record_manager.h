794bd9f5801e4df3cc95d9ea2bf514b9c6dd3582
John R. Patek <johnpatek2@gmail.com>
2020-11-08 22:51:04 -0800

    Started cache implementation

--- include/record_manager.h
+++ include/record_manager.h
@@ -2,10 +2,31 @@
 #include <string>
 #include <iomanip>
 #include <sstream>
+#include <list>
+#include <unordered_map>
+
 
 namespace rmp
 {
     std::string djb_hash(const std::string& data);
+
+    class record
+    {
+    private:
+        std::string _first_name;
+        std::string _last_name;
+        std::string _phone;
+    };
+
+    class request
+    {
+
+    };
+
+    class response
+    {
+
+    };
 
     class server
     {
@@ -13,6 +34,14 @@ namespace rmp
         server(uint16_t port);
         ~server();
     private:
+        struct hash
+        {
+            std::string operator()(const std::string& data)
+            {
+                return djb_hash(data);
+            }
+        };
+        std::unordered_map<std::string,std::string,hash> _cache;    
     };
 
     class client
